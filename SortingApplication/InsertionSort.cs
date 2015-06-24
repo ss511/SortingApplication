44f8d1ef3ac0df3f6bcdddc4d9d05b19a86f6b10
ss511 <shashank.s4u@gmail.com>
2015-06-24 22:31:12 +0530

    Iteration 3

--- SortingApplication/InsertionSort.cs
+++ SortingApplication/InsertionSort.cs
@@ -15,13 +15,13 @@ namespace SortingApplication
             for (int i = 1; i < n; i++)
             {
                 swap = arr[i];
-                int j = i - 1;
-                while(swap<arr[j] && j>=0)
+                int j = i;
+                while(j>0 && swap<arr[j-1] )
                 {
-                    arr[j+1] = arr[j];
+                    arr[j] = arr[j-1];
                     j--;
                 }
-                arr[j + 1] = swap;
+                arr[j] = swap;
             }
             Console.WriteLine("\nInsertion Sort:");
             for (int i = 0; i < n; i++)
